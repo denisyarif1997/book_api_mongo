58fe4badd1ce94f690e0b5457a8a332d0d10fe5a
Denis <48453257+denisyarif@users.noreply.github.com>
2023-06-23 15:17:17 +0700

    ok

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
+// app.js
 const express = require('express');
 const connectDB = require('./config/db');
-const bookRoutes = require('./routes/api/book')
+const cors = require('cors');
+
+// routes
+const books = require('./routes/api/book');
 
 const app = express();
-
-app.use(express.json());
 
 // Connect Database
 connectDB();
 
+// cors
+app.use(cors({ origin: true, credentials: true }));
 
-app.use('/api/books', bookRoutes);
+// Init Middleware
+app.use(express.json({ extended: false }));
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.get('/', (req, res) => res.send('Hello world!'));
+
+// use Routes
+app.use('/api/books', books);
+
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
